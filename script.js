d7e0d52a3c1f79bc245299ca207bb51c68fc78cf
IF2290 <246528729+IF2290@users.noreply.github.com>
2026-05-25 20:59:57 +0530

    Add basic layout to the document
    I added the name and I added the grids(which I added through javascript)
    I did this commit to make this the starting point.

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector("#container");
+for (let i = 0; i < 16; i++){
+    let columns = document.createElement("div");
+    columns.classList.add("columns");
+    for (let j = 0; j < 16; j++){
+        let column = document.createElement("div");
+        column.classList.add("column");
+        columns.appendChild(column);
+    };
+    container.appendChild(columns);
+};
