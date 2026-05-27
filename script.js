411c448feea58bf6bfeb8c0f5ab0467f224a4a6f
IF2290 <246528729+IF2290@users.noreply.github.com>
2026-05-27 11:59:00 +0530

    Make it so that div size will remain same even if no. of items in grid are changed and add measures to make sure that the number is an int and less tahn or equal to 100
    This makes it look much more usable.

--- script.js
+++ script.js
@@ -15,19 +15,25 @@ for (let i = 0; i < 16; i++){
 };
 
 button.addEventListener("click", (e) => {
-    let NumOfSquares = prompt("enter number of rows and columns (eg: 16 means 16x16 grid)")
-    container.replaceChildren();
-        for (let i = 0; i < NumOfSquares; i++){
-            let columns = document.createElement("div");
-            columns.classList.add("columns");
-            for (let j = 0; j < NumOfSquares; j++){
-                let column = document.createElement("div");
-                column.classList.add("column");
-                column.addEventListener("mouseover", (e) => {
-                column.setAttribute("style", "background-color: red;");
-            });
-        columns.appendChild(column);
-    };
-    container.appendChild(columns);
+    let NumOfSquares = prompt("enter number of rows and columns (eg: 16 means 16x16 grid)");
+    if (Number.isInteger(Number(NumOfSquares)) && NumOfSquares <= 100){
+        container.replaceChildren();
+            for (let i = 0; i < NumOfSquares; i++){
+                let columns = document.createElement("div");
+                columns.classList.add("columns");
+                for (let j = 0; j < NumOfSquares; j++){
+                    let column = document.createElement("div");
+                    column.classList.add("column");
+                    column.addEventListener("mouseover", (e) => {
+                    column.setAttribute("style", "background-color: red;");
+                });
+            columns.appendChild(column);
+        };
+        container.appendChild(columns);
+        };
+    } else if (NumOfSquares > 100){
+        alert("chill bro! the max is 100");
+    } else {
+        alert("Enter an integer only");
     };
 });
